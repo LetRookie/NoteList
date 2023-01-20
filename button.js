d265cc1e8c3e61209ab6484047f117c51b0b88a2
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-01-20 23:05:03 +0200

    new

---
+++ button.js
@@ -0,0 +1,9 @@
+function readText(){
+    let result = '';
+    let text = document.getElementById('input').value;
+    result += text;
+    document.getElementById('output').textContent += result+'\n';
+    //document.getElementById('sum').value = result;
+    let 
+
+}
